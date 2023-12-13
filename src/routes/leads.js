a460a0e993edd830184787dfeee722062b803431
Milton Jaimkes <20193tn028@utez.edu.mx>
2023-12-13 09:41:31 -0600

    troleito

--- src/routes/leads.js
+++ src/routes/leads.js
@@ -55,7 +55,7 @@ router.get('/:id', async (req, res) => {
   const { id } = req.params;
 
   try {
-    const lead = await pool.query('SELECT * FROM Leads WHERE LeadID = ?', [id]);
+    const lead = await pool.query('SELECT leads.LeadID, leads.NombreCompleto, leads.telefono,leads.telefono2, leads.CorreoElectronico, leads.CorreoElectronico2, leads.FechaPrimerContacto,leads.FechaNac, leads.EscuelaProcedencia, leads.NombrePais, leads.NombreEstado, leads.NombreCiudad, leads.PSeguimiento, leads.Grado,leads.EstatusInsc,leads.SemestreIngreso, leads.Ciclo, leads.AsetNameForm, leads.IsOrganic, leads.TipoReferido, leads.NombreReferido, leads.DondeObtDato, leads.FechaInscripcion, leads.BecaOfrecida, leads.NumeroLista, leads.FechaPromotorOriginal, leads.FechaPromotorActual, leads.Comentarios, leads.Programa, leads.FechaPromotorOriginal, CarrerasInt.Nombre as CarreraInteres,  Campana.Nombre as NombreCampana, MedioDeContacto.Nombre as MedioContacto, CarreraIns.Nombre as CarreraInscrita, PromotorOri.Nombre as NombrePromotorOri, PromotorAct.Nombre as NombrePromotorAct from leads LEFT JOIN Carreras CarrerasInt ON leads.carreraInteresID = CarrerasInt.CarreraID LEFT JOIN Campana ON leads.CampanaID = Campana.CampanaID LEFT JOIN MedioDeContacto ON leads.MedioDeContactoID = MedioDeContacto.MedioID LEFT JOIN Carreras CarreraIns ON leads.CarreraInscripcion = CarreraIns.CarreraID LEFT JOIN Promotor PromotorOri ON leads.PromotorOriginal = PromotorOri.PromotorID LEFT JOIN Promotor PromotorAct ON leads.PromotorActual = PromotorAct.PromotorID WHERE leads.LeadID = ?;', [id]);
     if (lead.length === 0) {
       return res.status(404).json({ error: 'Lead no encontrado' });
     }
@@ -120,9 +120,9 @@ router.put('/update-promotor/:id', async (req, res) => {
     return res.status(400).json({ error: 'El campo Promotor Original es obligatorio' });
   }
 
-  const fechaActual = new Date().toISOString().slice(0, 19).replace('T', ' ');  
-  const query = 'UPDATE Leads SET PromotorOriginal = ?, FechaPromotorOriginal = ? WHERE LeadID = ?';
-  const values = [PromotorOriginal,fechaActual, id];
+  const fechaActual = new Date().toISOString().slice(0, 19).replace('T', ' ');
+  const query = 'UPDATE Leads SET PromotorOriginal = ?, FechaPromotorOriginal = ?, PromotorActual = ?, FechaPromotorActual = ? WHERE LeadID = ?';
+  const values = [PromotorOriginal, fechaActual,PromotorOriginal, fechaActual, id];
 
   try {
     await pool.query(query, values);
