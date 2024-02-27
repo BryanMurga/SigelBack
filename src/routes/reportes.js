66dcbcb754042a7e2c38f2872a2308317d9b85ac
AldahirG <20173ti173@utez.edu.mx>
2024-02-27 12:29:16 -0600

    reportes ps added completed

--- src/routes/reportes.js
+++ src/routes/reportes.js
@@ -26,4 +26,37 @@ router.post('/reporte-ps-pi', async (req, res) => {
   }
 });
 
+// Obtener reporte de leads por promotores
+router.post('/reporte-ps-pi-promotores', async (req, res) => {
+  const { ciclo, fechaInicio, fechaFin } = req.body;
+
+  try {
+    const reportePromotores = await pool.query(`
+      SELECT
+        p.Nombre as Promotor,
+        COUNT(CASE WHEN l.PSeguimiento = 'PI-INSCRIPCIÓN' THEN 1 ELSE NULL END) as PI,
+        COUNT(CASE WHEN l.PSeguimiento = 'PI-INSCRIPCIÓN' AND l.FechaInscripcion IS NOT NULL THEN 1 ELSE NULL END) as INS_PI,
+        COUNT(CASE WHEN l.PSeguimiento = 'PS-SEGUIMIENTO' THEN 1 ELSE NULL END) as PS,
+        COUNT(CASE WHEN l.PSeguimiento = 'PS-SEGUIMIENTO' AND l.FechaInscripcion IS NOT NULL THEN 1 ELSE NULL END) as INS_PS,
+        COUNT(CASE WHEN l.PSeguimiento NOT IN ('PI-INSCRIPCIÓN', 'PS-SEGUIMIENTO') THEN 1 ELSE NULL END) as INS_OTRO_STATUS,
+        l.DondeObtDato as MedioOtraInscripcion,
+        COUNT(CASE WHEN l.FechaInscripcion IS NOT NULL THEN 1 ELSE NULL END) as TOTAL_INS
+      FROM Leads l
+      INNER JOIN Promotor p ON l.promotorActual = p.PromotorID
+      WHERE l.Ciclo = ? AND l.FechaPrimerContacto BETWEEN ? AND ?
+      GROUP BY p.Nombre, l.DondeObtDato
+    `, [ciclo, fechaInicio, fechaFin]);
+
+    res.json({
+      status: 200,
+      message: 'Reporte de Leads por Promotores obtenido correctamente',
+      reportePromotores: reportePromotores,
+    });
+  } catch (error) {
+    console.error('Error al obtener el reporte de Leads por Promotores:', error);
+    res.status(500).json({ error: 'Error en el servidor' });
+  }
+});
+
+
 module.exports = router;
